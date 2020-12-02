31f565d2d755db95f454c1b83dd77a649084ef91
hdaugaard99 <57973280+hdaugaard99@users.noreply.github.com>
2020-12-02 14:27:04 -0600

    Update offline.js

--- offline.js
+++ offline.js
@@ -1,4 +1,7 @@
 // JavaScript Document
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('offlinecache.js');
+};
 
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
