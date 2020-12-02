a928d5e9b3a5f0a793a85481d4f492607c41d17b
hdaugaard99 <57973280+hdaugaard99@users.noreply.github.com>
2020-12-02 13:23:49 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,15 +11,6 @@ function myFunction() {
   }
 }
 
-if ('serviceWorker' in navigator) {
-    console.log("Will the service worker register?");
-    navigator.serviceWorker.register('service-worker.js')
-      .then(function(reg){
-        console.log("Yes, it did.");
-     }).catch(function(err) {
-        console.log("No it didn't. This happened:", err)
-    });
- }
 
 
 if('serviceWorker' in navigator) {
