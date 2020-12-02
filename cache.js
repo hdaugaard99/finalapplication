d05278a875c0c628697fef115270588ed47a0bab
hdaugaard99 <57973280+hdaugaard99@users.noreply.github.com>
2020-12-02 14:02:17 -0600

    Update cache.js

--- cache.js
+++ cache.js
@@ -1,18 +1,9 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-    navigator.serviceWorker.register('sw.js').then(function(registration) {
-        // Registration successful
-
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-        // Registration failed
-
-        console.log('ServiceWorker registration failed: ', err);
-    });
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
 });
-};
 
-var cache = "cache";
+var cacheName = 'finalapplication';
+
 var urlsToCache = [
     '/',
     '/analysis.html',
@@ -30,27 +21,12 @@ var urlsToCache = [
     '/mediaqueries.css'
 ];
 
-self.addEventListener('install', function(event) {
-    // Perform install steps
-
-    event.waitUntil(
-        caches.open(cache) 
-        .then(function(cache) {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(response) {
-            // Cache hit - return response
-            if (response) {
-                return response;
-            }
-            return fetch(event.request);
-        })
-    );
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
